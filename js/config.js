d78009ee18c188f3dcb24571b93c0e2186ac6dba
BlackPigHe <hezijiang>
2021-07-31 15:23:58 +0800

    project create

---
+++ js/config.js
@@ -0,0 +1,4 @@
+window[window["TiktokAnalyticsObject"]].instance("C2ENC2QQV140ORDIO960").setAdvancedMatchingAvailableProperties({"email":true,"phone_number":false});
+window[window["TiktokAnalyticsObject"]].instance("C2ENC2QQV140ORDIO960").setPixelInfo && window[window["TiktokAnalyticsObject"]].instance("C2ENC2QQV140ORDIO960").setPixelInfo({status: 0, name: "SQSP Pixel", advertiserID: "6903975005743218690", setupMode: 1, partner: ""});
+window[window["TiktokAnalyticsObject"]].setPCMDomain && window[window["TiktokAnalyticsObject"]].setPCMDomain("squarespace.com");
+window[window["TiktokAnalyticsObject"]].setPCMConfig && window[window["TiktokAnalyticsObject"]].setPCMConfig([]);
